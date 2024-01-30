91c083ac977097323f811a189da956fe9d11d9db
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-01-29 20:05:36 -0700

    hug

--- codes/LLMs/Bloomz.py
+++ codes/LLMs/Bloomz.py
@@ -10,7 +10,7 @@ batch_size = 32
 def get_completion(prompt, model):
     inputs = tokenizer.encode(prompt, return_tensors="pt")
     outputs = model.generate(inputs)
-    return tokenizer.decode(outputs[0])
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 # Define a function to generate a completion for a given dataset
 def BloomzModel(dataset):
@@ -21,8 +21,9 @@ def BloomzModel(dataset):
         prompts = [f"""
         Replace [MASK] in following paragraph with one sentence that has a meaning similar to: {batch['sentence'][m]}. The paragraph is: {batch['paragraph'][m]}
         """ for m in range(len(batch))]
-        inputs = tokenizer.encode(prompts, return_tensors="pt")
+        inputs = tokenizer(prompts, return_tensors="pt")
         outputs.extend(model.generate(inputs))
         print(f"blooms {i} of {len(dataset)} done.")
+        assert len(outputs) == i + len(batch)
     return outputs
   
