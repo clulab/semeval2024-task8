4bf4c8d16c21f4abd9df544e3d19dc235c340fab
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-02-09 01:22:09 -0700

    e

--- train/utils.py
+++ train/utils.py
@@ -82,7 +82,7 @@ def prepare_dataset(model_name: str, setting: str, batch_size1: int = 16, tokeni
     #     train = train.filter(lambda example: example['id'] in ids_set)
         
     def tokenize_function(examples):
-       return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
+       return tokenizer(examples["paragraph"], truncation=True, padding="max_length", max_length=512)
     
     train = train.map(tokenize_function, batched=True, batch_size=batch_size1)
     val = val.map(tokenize_function, batched=True, batch_size=batch_size1)
