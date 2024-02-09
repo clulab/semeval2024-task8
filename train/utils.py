1a791fdd00bef15bb5ccf41bd0e7c0214df4ad91
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-02-09 01:10:28 -0700

    dsdsd:w

--- train/utils.py
+++ train/utils.py
@@ -61,14 +61,14 @@ def prepare_dataset(model_name: str, setting: str, batch_size1: int = 16, tokeni
     This function prepares the dataset.
     """
     # read the tokenized datasets
-    train = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_train.jsonl', split='train')
-    val = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_dev.jsonl', split='train')
-    test = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_test.jsonl', split='train')
-    # train = load_from_disk('../datasets/SubtaskB/train')
-    # val = load_from_disk('../datasets/SubtaskB/dev')
-    # test = load_from_disk('../datasets/SubtaskB/test')
+    # train = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_train.jsonl', split='train')
+    # val = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_dev.jsonl', split='train')
+    # test = load_dataset('json', data_files='../datasets/SubtaskB/subtaskB_test.jsonl', split='train')
+    train = load_from_disk('../datasets/SubtaskB/train')
+    val = load_from_disk('../datasets/SubtaskB/dev')
+    test = load_from_disk('../datasets/SubtaskB/test')
     
-    test = test.add_column('label', [100] * len(test))
+    # test = test.add_column('label', [100] * len(test))
 
     if ids_set is not None:
         # print(type(ids_set))
