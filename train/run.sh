a163b38377412d38307c5d16368f05476e6ff135
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-02-14 11:46:54 -0700

    try

--- train/run.sh
+++ train/run.sh
@@ -13,6 +13,7 @@ check_jobs() {
   done
 }
 
+
 # Loop through inputs and execute them
 for i in "${!inputs[@]}"; do
   # Check if we need to wait for a job slot to become available
@@ -20,9 +21,12 @@ for i in "${!inputs[@]}"; do
   
   # Calculate GPU index: i % MAX_JOBS ensures cycling through GPUs 0 to MAX_JOBS-1
   gpu_index=$((i % MAX_JOBS))
+  
+  # Wait for 10 seconds before starting the next script
+  sleep 10
 
   # Execute the script with CUDA_VISIBLE_DEVICES set for the specific GPU
-  CUDA_VISIBLE_DEVICES=$gpu_index python ./myTrain2.py "${inputs[$i]}" &
+  CUDA_VISIBLE_DEVICES=$gpu_index python code/train2.py "${inputs[$i]}" &
 done
 
 # Wait for all background jobs to finish
